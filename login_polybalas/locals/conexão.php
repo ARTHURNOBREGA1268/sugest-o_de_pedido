29163b58db5b3bf2fe4ab6ed44bf8a56a973a947
ArthurNobrega <136469756+ARTHURNOBREGA1268@users.noreply.github.com>
2024-05-10 15:18:09 -0300

    Add files via upload

---
+++ login_polybalas/locals/conexão.php
@@ -0,0 +1,21 @@
+<?php
+
+// Informações de conexão
+$host = "localhost";
+$port = "5432";
+$dbname = "polybalas"; // Nome do banco de dados
+$user = "postgres";
+$password = "1234mudar"; // Senha do banco de dados
+
+try {
+    // Tentativa de conexão
+    $conn = new PDO("pgsql:host=$host;port=$port;dbname=$dbname;user=$user;password=$password");
+
+    // Define para que o PDO lance exceções em caso de erro
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo "Conexão efetuada com sucesso!";
+} catch(PDOException $e) {
+    die("Erro na conexão: " . $e->getMessage());
+}
+?>
